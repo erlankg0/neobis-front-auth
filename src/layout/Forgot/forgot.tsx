03a7f57f2441b9ed7b4a5681d807a20d4289f44a
Erlan Abdraimov <62596605+erlankg0@users.noreply.github.com>
2024-05-04 23:06:55 +0300

    fix bugs

--- src/layout/Forgot/forgot.tsx
+++ src/layout/Forgot/forgot.tsx
@@ -28,7 +28,7 @@ const Forgot = () => {
             dispatch(clear());
             navigate(`/resetPassword?token=${token}`)
         } else {
-            console.log(null)
+            navigate('/')
         }
     }, [searchParams])
 
